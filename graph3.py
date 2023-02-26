86767b44f8f811d504c63216d41fb00c83ec8afa
NooberPlays <62673966+NooberPlays@users.noreply.github.com>
2023-02-25 23:47:07 -0600

    .

--- graph3.py
+++ graph3.py
@@ -25,32 +25,19 @@ def plot_function_with_pattern(func, x_range, y_range, base_size, shape="rectang
     if start_y is None:
         start_y = y_range[0]
 
+
     for x_i in np.arange(start_x, x_range[1], base_size):
         for y_i in np.arange(start_y, y_range[1], base_size):
             if func(x_i) >= y_i:
                 # Create shape patch
                 if shape == "rectangle":
-                    patch = Rectangle((x_i, y_i), base_size, base_size, facecolor='green')
+                    patch = Rectangle((x_i, y_i), base_size, radius=base_size, facecolor='green')
                 elif shape == "triangle":
-                    patch = RegularPolygon((x_i+base_size/2, y_i), 3, base_size/np.sqrt(3), orientation=0, facecolor='green')
+                    patch = RegularPolygon((x_i+base_size/2, y_i), 3, radius=base_size/np.sqrt(3), orientation=0, facecolor='green')
                 elif shape == "square":
-                    patch = RegularPolygon((x_i+base_size/2, y_i+base_size/2), 4, base_size/np.sqrt(2), orientation=np.pi/4, facecolor='green')
+                    patch = RegularPolygon((x_i+base_size/2, y_i+base_size/2), 4, radius=base_size/np.sqrt(2), orientation=np.pi/4, facecolor='green')
                 elif shape == "hexagon":
-                    patch = RegularPolygon((x_i+base_size/2, y_i), 6, base_size/np.sqrt(3), orientation=np.pi/2, facecolor='green')
-                # Add patch to plot
-                ax.add_patch(patch)
-                num_figures += 1
-            elif include_touching and func(x_i) == y_i:
-                # Create shape patch with transparency
-                if shape == "rectangle":
-                    patch = Rectangle((x_i, y_i), base_size, base_size, facecolor='red')
-                elif shape == "triangle":
-                    patch = RegularPolygon((x_i+base_size/2, y_i), 3, base_size/np.sqrt(3), orientation=0, facecolor='red')
-                elif shape == "square":
-                    patch = RegularPolygon((x_i+base_size/2, y_i+base_size/2), 4, base_size/np.sqrt(2), orientation=np.pi/4, facecolor='red')
-                elif shape == "hexagon":
-                    patch = RegularPolygon((x_i+base_size/2, y_i), 6, base_size/np.sqrt(3), orientation=np.pi/2, facecolor='red')
-                # Add patch to plot
+                    patch = RegularPolygon((x_i+base_size/2, y_i), 6, radius=base_size/np.sqrt(3), orientation=np.pi/2, facecolor='green')
                 ax.add_patch(patch)
                 num_figures += 1
 
@@ -65,4 +52,4 @@ def plot_function_with_pattern(func, x_range, y_range, base_size, shape="rectang
 def func(x):
     return -0.00002*x**6 + 0.0011*x**5 -0.024 *x**4 + 0.24*x**3 -0.8*x**2 -x + 10
 
-plot_function_with_pattern(func, x_range=(0, 19.308), y_range=(0, 17.55), base_size=1, shape="hexagon", include_touching=True)
+plot_function_with_pattern(func, x_range=(0, 19.308), y_range=(0, 17.55), base_size=1, shape="hexagon")
